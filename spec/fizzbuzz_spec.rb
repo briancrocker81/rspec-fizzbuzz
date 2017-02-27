4b3caac122e970cc9eb2e882c5d861842b2a7554
Brian Crocker <brian.crocker@vualto.com>
2017-02-27 20:57:14 +0000

    added tests for 3 and 5

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,11 +5,17 @@ describe 'fizzbuzz' do
     it 'is divisible by 3' do
       expect(is_divisible_by_three?(3)).to be_truthy
     end
+    it 'is divisible by 5' do
+      expect(is_divisible_by_five?(5)).to be_truthy
+    end
   end
 
   context 'knows a number is not divisible by' do
     it 'is not divisible by 3' do
       expect(is_divisible_by_three?(1)).not_to be_truthy
+    end
+    it 'is not divisible by 5' do
+      expect(is_divisible_by_five?(1)).not_to be_truthy
     end
   end
 
